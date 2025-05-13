a9e3d1ee107ed24dcd546d77cc345cf64cac07aa
Facundo S <sarfacundo10@gmail.com>
2025-05-13 19:31:29 -0300

    Ejercicio 1.1 Finalizado

--- Part 1/coursinfo/src/App.jsx
+++ Part 1/coursinfo/src/App.jsx
@@ -1,3 +1,24 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Numero de Ejercicio{props.total}</p>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1}{props.exercises1} </p>
+      <p>{props.part2}{props.exercises2} </p>
+      <p>{props.part3}{props.exercises3}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Desarrollo de aplicaciones Half Stack'
   const part1 = 'Fundamentos de React'
@@ -8,19 +29,11 @@ const App = () => {
   const exercises3 = 14
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Numero de Ejercicio{exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} /> 
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
-}
+  }
   
 export default App
