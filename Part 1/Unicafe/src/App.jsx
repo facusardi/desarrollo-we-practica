d7ca3dfcd557c649c6a66def2a73a839d6a13dee
Facundo S <sarfacundo10@gmail.com>
2025-05-27 19:52:30 -0300

    Ejercicio 1.7 Finalizado

--- Part 1/Unicafe/src/App.jsx
+++ Part 1/Unicafe/src/App.jsx
@@ -6,16 +6,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [malo, setBad] = useState(0)
 
+  const getAll = () => bueno + neutral + malo
+  const getProm = () => (bueno - malo) / getAll() 
+  const getDisparo = () => bueno*100/getAll()
   return (
     <div>
       <h1>Dar Retroalimentacion</h1>
-      <button onClick={() => setGood(bueno + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(malo + 1)}>bad</button>
+      <button onClick={() => setGood(bueno + 1)}>Bueno</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(malo + 1)}>Malo</button>
       <h1>Estadistica</h1>
-      <p>Bueno {bueno}</p>
-      <p>Neutral {neutral}</p>
-      <p>Malo {malo}</p>
+      <p>Comentarios Buenos: {bueno}</p>
+      <p>Comentarios Neutrales: {neutral}</p>
+      <p>Comentarios Malos: {malo}</p>
+      <p>Todos los Comentarios: {getAll()}</p>
+      <p>Promedio de Comentarios: {getProm()}%</p>
+      <p>Porcentaje de Comentarios Buenos: {getDisparo()}%</p>
     </div>
   )
 }
