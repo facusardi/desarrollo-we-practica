9600e98a34a0b057c2fe56aff3183846fb9859bb
Facundo S <sarfacundo10@gmail.com>
2025-05-27 21:12:50 -0300

    Ejercicio 1.9 Finalizado

--- Part 1/Unicafe/src/Componentes/Stadistics.jsx
+++ Part 1/Unicafe/src/Componentes/Stadistics.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 const Stadistic = ({votes}) => {
 
   const getAll = () => votes.bueno + votes.neutral + votes.malo
-  const getProm = () =>getAll()===0 ? 0 : (votes.bueno - votes.malo) / getAll() 
-  const getDisparo = () =>getAll()===0 ? 0 : votes.bueno*100 / getAll()
+  const getProm = () => (votes.bueno - votes.malo) / getAll() 
+  const getDisparo = () => votes.bueno*100 / getAll()
 
   return (
+    getAll()=== 0 ? <p>No hay retroalimentacion</p> :
     <div>
-        <h1>Estadistica</h1>
+     
+       <h1>Estadistica</h1>
       <p>Comentarios Buenos: {votes.bueno}</p>
       <p>Comentarios Neutrales: {votes.neutral}</p>
       <p>Comentarios Malos: {votes.malo}</p>
