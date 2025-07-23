a82da9b35f4e6e6e15409f62eff9f2bb690ed708
Facundo S <sarfacundo10@gmail.com>
2025-07-23 12:19:03 -0300

    Ejercicio 2.7 Finalizado

--- Part 2/formularios/src/App.jsx
+++ Part 2/formularios/src/App.jsx
@@ -10,6 +10,12 @@ function App() {
 
   const addPerson = (event) => {
     event.preventDefault();
+    
+   if (persons.some(person => person.name === newName)) 
+    {
+    alert(`${newName} is already added to phonebook`);
+    return; 
+    }
     const personObject = { name: newName };
     setPersons(persons.concat(personObject));
     setNewName('');
