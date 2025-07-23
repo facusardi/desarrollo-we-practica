1cf5ec56a967bf29115c37c082c2fba08485b93b
Facundo S <sarfacundo10@gmail.com>
2025-07-23 11:59:30 -0300

    Actividad 2.6 Finalizada

---
+++ Part 2/formularios/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function App() {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = { name: newName };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div> 
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((p, i) => (
+          <li key={i}>{p.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
